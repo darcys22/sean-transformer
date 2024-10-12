701b8f1bcf6b0329f9c76052f0dd6926c32c72ea
Sean Darcy <darcys22@gmail.com>
2024-10-12 18:10:39 +1100

    inital commit

---
+++ model.py
@@ -0,0 +1,94 @@
+import math
+from dataclasses import dataclass
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+torch.set_printoptions(sci_mode=False)
+
+@dataclass
+class ModelArgs:
+    emb_size: int = 512
+    n_layers: int = 16
+    n_heads: int = 16
+    seq_length: int = 512
+    BS: int = 32
+    vocab_size: int = 64
+
+class MLP(nn.Module):
+    def __init__(self, emb_length):
+        super().__init__()
+        self.i2h = nn.Linear(emb_length, emb_length * 4)
+        self.h2o = nn.Linear(emb_length * 4, emb_length)
+
+    def forward(self, x):
+        x = F.relu(self.i2h(x))
+        x = self.h2o(x)
+        return x
+
+class ScaledDotProductAttention(nn.Module):
+    def __init__(self, emb_length, n_heads):
+        super().__init__()
+        self.head_size = emb_length // n_heads
+        self.key = nn.Linear(self.head_size, self.head_size)
+        self.query = nn.Linear(self.head_size, self.head_size)
+        self.value = nn.Linear(self.head_size, self.head_size)
+        self.n_heads = n_heads
+        self.ln1 = nn.LayerNorm(self.head_size)
+        self.ln2 = nn.LayerNorm(self.head_size)
+
+    def forward(self, x):
+        xq = self.query(x)
+        xk = self.key(x)
+        xv = self.value(x)
+        att = (xq @ xk.transpose(-2,-1)) * (1.0 / math.sqrt(self.n_heads))
+        # TODO sean mask here
+        x = F.softmax(att, dim=-1)
+        x = x @ xv
+        return x
+
+class MultiHeadAttention(nn.Module):
+    def __init__(self, args: ModelArgs):
+        super().__init__()
+        self.n_heads = args.n_heads
+        self.emb_size = args.emb_size
+        self.heads = nn.ModuleList([ScaledDotProductAttention(args.emb_size, args.n_heads) for i in range(args.n_heads)])
+        self.feed_forward = MLP(args.emb_size)
+
+    def forward(self, x):
+        x = torch.split(x, self.emb_size // self.n_heads, 2)
+        attention_heads = []
+        for i in range(self.n_heads):
+            head_x = self.heads[i](x[i])
+            attention_heads.append(head_x)
+        x = torch.cat(attention_heads, 2)
+        x = self.feed_forward(x)
+        return x
+
+class Transformer(nn.Module):
+    def __init__(self, args: ModelArgs):
+        super().__init__()
+        self.embedding = nn.Embedding(args.vocab_size, args.emb_size)
+        self.positional_encoding = nn.Embedding(args.seq_length, args.emb_size)
+        self.layers = nn.ModuleList([MultiHeadAttention(args) for i in range(args.n_layers)])
+        self.n_layers = args.n_layers
+        self.seq_length = args.seq_length
+        self.drop = nn.Dropout(p=0.2)
+        self.ln = nn.LayerNorm(args.emb_size)
+        self.ll = nn.Linear(args.emb_size, args.vocab_size)
+        self.embedding.weight = self.ll.weight
+
+
+    def forward(self, x):
+        b, t = x.size()
+        tok_emb = self.embedding(x)
+        assert t <= self.seq_length, f"Cannot forward sequence of length {t}, block size is only {self.seq_length}"
+        pos = torch.arange(0, t, dtype=torch.long, device=x.device) # shape (t)
+        pos_emb = self.positional_encoding(pos) # position embeddings of shape (t, n_embd)
+        x = self.drop(tok_emb + pos_emb)
+        for transformer_block in self.layers:
+            x = transformer_block(x)
+        x = self.ln(x)
+        x = self.ll(x)
+        x = F.softmax(x, dim=-1)
+        return x
---
+++ train.py
@@ -0,0 +1,105 @@
+import math
+import random
+import os
+
+import numpy as np
+np.set_printoptions(suppress=True)
+import matplotlib.pyplot as plt
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+torch.set_printoptions(sci_mode=False)
+from torch.utils.data import Dataset, DataLoader
+from torchvision import datasets, transforms
+
+import requests
+from tqdm import tqdm, trange
+
+import model
+
+# attempt to autodetect device
+device = "cpu"
+if torch.cuda.is_available():
+    device = "cuda"
+elif hasattr(torch.backends, "mps") and torch.backends.mps.is_available():
+    device = "mps"
+print(f"using device: {device}")
+
+# Get default arguments
+model_args: model.ModelArgs = model.ModelArgs()
+
+# Custom dataset class for Tiny Shakespeare
+class TinyShakespeareDataset(Dataset):
+    def __init__(self, text, seq_length):
+        self.text = text
+        self.seq_length = seq_length
+        self.chars = sorted(list(set(text)))
+        self.char_to_idx = {ch: i for i, ch in enumerate(self.chars)}
+        self.idx_to_char = {i: ch for i, ch in enumerate(self.chars)}
+        self.data_size = len(self.text)
+
+    def __len__(self):
+        return max(0, self.data_size - self.seq_length)
+
+    def __getitem__(self, index):
+        x = [self.char_to_idx[c] for c in self.text[index:index+self.seq_length]]
+        y = [self.char_to_idx[c] for c in self.text[index+1:index+self.seq_length+1]]
+        return torch.tensor(x), torch.tensor(y)
+
+# Download Tiny Shakespeare dataset
+def download_tiny_shakespeare():
+    url = "https://raw.githubusercontent.com/karpathy/char-rnn/master/data/tinyshakespeare/input.txt"
+    if not os.path.exists("data/tinyshakespeare.txt"):
+        data = requests.get(url).text
+        with open("data/tinyshakespeare.txt", "w") as f:
+            f.write(data)
+
+# Download the dataset
+download_tiny_shakespeare()
+
+# Read the dataset
+with open("data/tinyshakespeare.txt", "r") as f:
+    text = f.read()
+
+# Create dataset and split into train and test
+dataset = TinyShakespeareDataset(text, model_args.seq_length)
+model_args.vocab_size = len(dataset.chars)
+train_size = int(0.8 * len(dataset))
+test_size = len(dataset) - train_size
+train_dataset, test_dataset = torch.utils.data.random_split(dataset, [train_size, test_size])
+
+# Create data loaders
+loaders = {
+    'train': DataLoader(train_dataset, batch_size=model_args.BS, shuffle=True),
+    'test': DataLoader(test_dataset, batch_size=model_args.BS, shuffle=True),
+}
+
+# Get model
+seanTransformer = model.Transformer(model_args)
+seanTransformer.to(device)
+
+criterion = nn.CrossEntropyLoss()
+optim = torch.optim.Adam(seanTransformer.parameters(), lr = 6e-4)
+
+num_epochs = 50
+train_losses = {}
+
+for epoch in range(num_epochs):
+    epoch_losses = list()
+    for i, (X, Y) in enumerate(loaders['train']):
+        if X.shape[0] != model_args.BS:
+            continue
+        optim.zero_grad()
+        X, Y, = X.to(device), Y.to(device)
+        out = seanTransformer(X)
+        out = out.transpose(1, 2)  # batch_size x vocab_size
+        loss = criterion(out, Y.long())
+        loss.backward()
+        optim.step()
+
+        epoch_losses.append(loss.detach().item() / X.shape[1])
+        if (i+1) % 250 == 0:
+            print('Loss: {:.4f}'.format(loss.detach().item()))
+    train_losses[epoch] = torch.tensor(epoch_losses).mean()
+    print(f'=> epoch: {epoch + 1}, loss: {train_losses[epoch]}')
